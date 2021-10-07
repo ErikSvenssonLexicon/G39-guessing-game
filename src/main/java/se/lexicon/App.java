88e3bba650a170a51d5b8de0ae7107e8d257ddef
Erik Svensson <eosvensson@gmail.com>
2021-10-07 09:52:12 +0200

    Initialized project

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+/**
+ * Guessing Game
+ * @author Erik Svensson
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
